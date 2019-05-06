1f06d7737fcd9e4a02f407aca894611b7fc6efd6
Marco Kuan <kuan.n.marco@gmail.com>
2019-05-06 04:44:58 -0700

    add coActors

--- src/SixDegrees.java
+++ src/SixDegrees.java
@@ -2,32 +2,36 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SixDegrees {
-  public static void main(String[] args) {
-    String path = args[0];
+  /** Stores the actor -> worked with actors */
+  public HashMap<String, Set<String>> actors;
 
-    try {
-      storeNames(path);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    System.out.println("end");
+  public SixDegrees() {
+    actors = new HashMap<>();
   }
 
-  public static void storeNames(String file) throws FileNotFoundException, IOException {
-    List<String> records = new ArrayList<>();
+  /**
+   * Reads the names of the actors in a file
+   * 
+   * @param file the file that is read
+   * @throws FileNotFoundException
+   * @throws IOException
+   */
+  public void storeNames(String file) throws FileNotFoundException, IOException {
 
     try (BufferedReader br = new BufferedReader(new FileReader(file))) {
       String line;
 
       // Read each line
       while ((line = br.readLine()) != null) {
+        Set<String> coActors = new HashSet<>();
+
         String[] values = line.split(",");
 
         for (String val : values) {
@@ -37,17 +41,36 @@ public class SixDegrees {
 
             // Regex add names
             while (m.find()) {
-              records.add(m.group(1));
+              coActors.add(m.group(1));
             }
           } else if (val.contains("}]")) {
+            // Add coActors associated with the actor
+            for (String actor : coActors) {
+              actors.put(actor, coActors);
+
+//              actors.get(actor).remove(actor);
+            }
+
             break; // Avoid crews
           }
         }
       }
     }
+  }
 
-    for (String names : records) {
-      System.out.print(names + " ");
+  public static void main(String[] args) {
+    String path = args[0];
+    SixDegrees SD = new SixDegrees();
+
+    try {
+      SD.storeNames(path);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+    for (String actor : SD.actors.keySet()) {
+      System.out.println(actor + " = " + SD.actors.get(actor));
+    }
+    System.out.println();
   }
 }
